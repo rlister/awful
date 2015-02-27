68a937acf1bc9bca379731a36e5fa43e7416bde9
Richard Lister <rlister+gh@gmail.com>
2015-02-27 17:42:07 -0500

    shared library code

--- lib/awsm.rb
+++ lib/awsm.rb
@@ -1,5 +1,53 @@
-require "awsm/version"
+require 'awsm/version'
+
+require 'aws-sdk'
+require 'thor'
+require 'yaml'
 
 module Awsm
-  # Your code goes here...
+
+  def ec2
+    @ec2 ||= Aws::EC2::Client.new
+  end
+
+  def autoscaling
+    @autoscaling ||= Aws::AutoScaling::Client.new
+  end
+
+  def symbolize_keys(thing)
+    if thing.is_a?(Hash)
+      Hash[ thing.map { |k,v| [ k.to_sym, symbolize_keys(v) ] } ]
+    elsif thing.respond_to?(:map)
+      thing.map { |v| symbolize_keys(v) }
+    else
+      thing
+    end
+  end
+
+  def stringify_keys(thing)
+    if thing.is_a?(Hash)
+      Hash[ thing.map { |k,v| [ k.to_s, stringify_keys(v) ] } ]
+    elsif thing.respond_to?(:map)
+      thing.map { |v| stringify_keys(v) }
+    else
+      thing
+    end
+  end
+
+  def load_cfg
+    $stdin.tty? ? {} : symbolize_keys(YAML.load($stdin.read))
+  end
+
+  def only_keys_matching(hash, keylist)
+    hash.select do |key,_|
+      keylist.include?(key)
+    end
+  end
+
+  def remove_empty_strings(hash)
+    hash.reject do |_,value|
+      value.respond_to?(:empty?) and value.empty?
+    end
+  end
+
 end
