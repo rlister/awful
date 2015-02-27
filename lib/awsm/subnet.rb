ae03eb9bd939e6c63cd22492ac74568189a92fed
Richard Lister <rlister+gh@gmail.com>
2015-02-27 17:59:19 -0500

    very basic vpc and subnet support

---
+++ lib/awsm/subnet.rb
@@ -0,0 +1,32 @@
+module Awsm
+
+  class Subnet < Thor
+    include Awsm
+
+    desc 'ls [PATTERN]', 'list subnets [with any tags matching PATTERN]'
+    method_option :long, aliases: '-l', default: false, desc: 'Long listing'
+    def ls(name = /./)
+      fields = options[:long] ?
+        ->(s) { [s.tags.map{ |t| t.value }.join(','), s.subnet_id, s.state, s.cidr_block, s.available_ip_address_count, s.availability_zone] } :
+        ->(s) { [s.subnet_id] }
+      ec2.describe_subnets.map(&:subnets).flatten.select do |subnet|
+        subnet.tags.any? { |tag| tag.value.match(name) }
+      end.tap{ |s| p s }.map do |subnet|
+        fields.call(subnet)
+      end.tap do |list|
+        print_table list.sort
+      end
+    end
+
+    desc 'dump NAME', 'dump subnet with id or tag NAME as yaml'
+    def dump(name)
+      ec2.describe_subnets.map(&:subnets).flatten.find do |subnet|
+        subnet.subnet_id == name or subnet.tags.any? { |tag| tag.value == name }
+      end.tap do |subnet|
+        puts YAML.dump(stringify_keys(subnet.to_hash))
+      end
+    end
+
+  end
+
+end
---
+++ lib/awsm/vpc.rb
@@ -0,0 +1,32 @@
+module Awsm
+
+  class Vpc < Thor
+    include Awsm
+
+    desc 'ls [PATTERN]', 'list vpcs [with any tags matching PATTERN]'
+    method_option :long, aliases: '-l', default: false, desc: 'Long listing'
+    def ls(name = /./)
+      fields = options[:long] ?
+        ->(v) { [v.tags.map{ |t| t.value }.join(','), v.vpc_id, v.state, v.cidr_block] } :
+        ->(v) { [v.vpc_id] }
+      ec2.describe_vpcs.map(&:vpcs).flatten.select do |vpc|
+        vpc.tags.any? { |tag| tag.value.match(name) }
+      end.map do |vpc|
+        fields.call(vpc)
+      end.tap do |list|
+        print_table list
+      end
+    end
+
+    desc 'dump NAME', 'dump VPC with id or tag NAME as yaml'
+    def dump(name)
+      ec2.describe_vpcs.map(&:vpcs).flatten.find do |vpc|
+        vpc.vpc_id == name or vpc.tags.any? { |tag| tag.value == name }
+      end.tap do |vpc|
+        puts YAML.dump(stringify_keys(vpc.to_hash))
+      end
+    end
+
+  end
+
+end
