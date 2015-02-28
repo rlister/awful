f4180fe7b57eae4f3400a2f12dcd9339bbdcfb27
Richard Lister <rlister+gh@gmail.com>
2015-02-27 22:08:52 -0500

    add create and delete for ELB

--- lib/awsm/elb.rb
+++ lib/awsm/elb.rb
@@ -38,6 +38,25 @@ module Awsm
       puts YAML.dump(stringify_keys(lb))
     end
 
+    desc 'create NAME', 'create new load-balancer'
+    def create(name)
+      whitelist = %i[load_balancer_name listeners availability_zones subnets security_groups scheme tags]
+      opt = load_cfg
+      opt[:load_balancer_name] = name
+      opt[:listeners] = opt.fetch(:listener_descriptions, []).map { |l| l[:listener] }
+      opt.delete(:availability_zones) unless opt.fetch(:subnets, []).empty?
+      opt = remove_empty_strings(opt)
+      opt = only_keys_matching(opt, whitelist)
+      elb.create_load_balancer(opt)
+    end
+
+    desc 'delete NAME', 'delete load-balancer'
+    def delete(name)
+      if yes? "really delete ELB #{name}?", :yellow
+        elb.delete_load_balancer(load_balancer_name: name)
+      end
+    end
+
   end
 
 end
