1ee1db584e088c50a9c04898a30e08ca191cf1da
Richard Lister <rlister+gh@gmail.com>
2015-03-25 13:27:01 -0400

    allow launch config name as second arg

--- lib/awful/launch_config.rb
+++ lib/awful/launch_config.rb
@@ -53,11 +53,11 @@ module Awful
       end
     end
 
-    desc 'create [NAME]', 'create a new launch configuration'
+    desc 'create NAME [FILE]', 'create a new launch configuration'
     method_option :timestamp, aliases: '-t', default: false, desc: 'Add timestamp to launch config name'
     method_option :patch,     aliases: '-p', default: false, desc: 'Add release to launch config name'
-    def create(name)
-      opt = load_cfg(options)
+    def create(name, file = nil)
+      opt = load_cfg(options, file)
 
       whitelist = %i[launch_configuration_name image_id key_name security_groups classic_link_vpc_id classic_link_vpc_security_groups user_data
                      instance_id instance_type kernel_id ramdisk_id block_device_mappings instance_monitoring spot_price iam_instance_profile
