c9116df7662d12e74a7affe8588527235bc9f4d9
Richard Lister <rlister+gh@gmail.com>
2015-06-04 15:46:30 -0400

    abstract out the images method and make owners a class option

--- lib/awful/ami.rb
+++ lib/awful/ami.rb
@@ -1,16 +1,22 @@
 module Awful
 
   class Ami < Cli
+    class_option :owners, aliases: '-o', default: 'self', desc: 'List images with this owner'
+
+    no_commands do
+      def images(options)
+        ec2.describe_images(owners: options[:owners].split(',')).map(&:images).flatten
+      end
+    end
 
     desc 'ls [PATTERN]', 'list AMIs'
     method_option :long, aliases: '-l', default: false, desc: 'Long listing'
-    method_option :owners, aliases: '-o', default: 'self', desc: 'List images with this owner'
     def ls(name = /./)
       fields = options[:long] ?
         ->(i) { [ i.name, i.image_id, i.root_device_type, i.state, i.creation_date, i.tags.map{ |t| "#{t.key}=#{t.value}" }.sort.join(',') ] } :
         ->(i) { [ i.image_id ] }
 
-      ec2.describe_images(owners: options[:owners].split(',')).map(&:images).flatten.select do |image|
+      images(options).select do |image|
         image.name.match(name)
       end.map do |image|
         fields.call(image)
