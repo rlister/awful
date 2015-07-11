b5703380ee7adf05a037e916b1ec82c40d6a3575
Richard Lister <rlister+gh@gmail.com>
2015-07-11 15:15:00 -0400

    add pipeline id to long output

--- lib/awful/datapipeline.rb
+++ lib/awful/datapipeline.rb
@@ -14,9 +14,9 @@ module Awful
         datapipeline.describe_pipelines(pipeline_ids: dps.map(&:id)).pipeline_description_list.map do |dp|
           dp.fields.each_with_object({}) do |f, h|
             h[f.key] = f.string_value # convert array of structs to hash
-          end
+          end.merge('id' => dp.pipeline_id)
         end.tap do |list|
-          print_table list.map { |d| d.values_at('@creationTime', 'name', '@pipelineState', '@healthStatus') }.sort
+          print_table list.map { |d| d.values_at('@creationTime', 'id', '@pipelineState', '@healthStatus', 'name') }.sort
         end
       else
         puts dps.map(&:name).sort
