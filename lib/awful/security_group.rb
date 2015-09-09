89095d57cc9a630682dc9f37fc69b2657b792446
Richard Lister <rlister+gh@gmail.com>
2015-09-09 16:38:12 -0400

    add inbound rule task

--- lib/awful/security_group.rb
+++ lib/awful/security_group.rb
@@ -27,6 +27,25 @@ module Awful
       end
     end
 
+    desc 'inbound NAME', 'show inbound rules for named security group'
+    method_option :long, aliases: '-l', default: false, desc: 'Long listing'
+    def inbound(name)
+      ec2.describe_security_groups.map(&:security_groups).flatten.find do |sg|
+        sg.group_name == name
+      end.ip_permissions.tap do |perms|
+        sources = ->(perm) { perm.ip_ranges.map(&:cidr_ip) + perm.user_id_group_pairs.map(&:group_id) }
+        if options[:long]
+          perms.map do |p|
+            sources.call(p).map do |s|
+              [p.ip_protocol, p.from_port, p.to_port, s]
+            end
+          end.flatten(1).tap { |list| print_table list }
+        else
+          puts perms.map { |p| sources.call(p) }.flatten
+        end
+      end
+    end
+
   end
 
 end
