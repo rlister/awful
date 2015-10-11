b82cf60b921a489302853925efce629ef17afc70
Richard Lister <rlister+gh@gmail.com>
2015-10-11 14:13:14 -0400

    long vpc listing now shows just Name tag instead of all tags

--- lib/awful/vpc.rb
+++ lib/awful/vpc.rb
@@ -6,7 +6,7 @@ module Awful
     method_option :long, aliases: '-l', default: false, desc: 'Long listing'
     def ls(name = /./)
       fields = options[:long] ?
-        ->(v) { [v.tags.map{ |t| t.value }.join(','), v.vpc_id, v.state, v.cidr_block] } :
+        ->(v) { [tag_name(v), v.vpc_id, v.state, v.cidr_block] } :
         ->(v) { [v.vpc_id] }
       ec2.describe_vpcs.map(&:vpcs).flatten.select do |vpc|
         vpc.tags.any? { |tag| tag.value.match(name) }
