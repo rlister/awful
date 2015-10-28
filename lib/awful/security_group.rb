97b13480b522d6e84cfa34867bbf6a02a4911ec7
Richard Lister <rlister+gh@gmail.com>
2015-10-28 10:25:59 -0400

    dump and inbound now match first group_id, group_name or tag:Name

--- lib/awful/security_group.rb
+++ lib/awful/security_group.rb
@@ -18,11 +18,21 @@ module Awful
       end
     end
 
-    desc 'dump NAME', 'dump security group with NAME as yaml'
+    no_commands do
+
+      ## return first SG that matches name to id, group_name, or Name tag
+      def first_matching_sg(name)
+        field = name.match(/^sg-[\d[a-f]]{8}$/) ? :group_id : :group_name
+        ec2.describe_security_groups.map(&:security_groups).flatten.find do |sg|
+          sg.send(field).match(name) or (tag_name(sg)||'').match(name)
+        end
+      end
+
+    end
+
+    desc 'dump NAME', 'dump security group with NAME [or ID] as yaml'
     def dump(name)
-      ec2.describe_security_groups.map(&:security_groups).flatten.find do |sg|
-        sg.group_name == name
-      end.tap do |sg|
+      first_matching_sg(name).tap do |sg|
         puts YAML.dump(stringify_keys(sg.to_hash))
       end
     end
@@ -30,9 +40,7 @@ module Awful
     desc 'inbound NAME', 'show inbound rules for named security group'
     method_option :long, aliases: '-l', default: false, desc: 'Long listing'
     def inbound(name)
-      ec2.describe_security_groups.map(&:security_groups).flatten.find do |sg|
-        sg.group_name == name
-      end.ip_permissions.tap do |perms|
+      first_matching_sg(name).ip_permissions.tap do |perms|
         sources = ->(perm) { perm.ip_ranges.map(&:cidr_ip) + perm.user_id_group_pairs.map(&:group_id) }
         if options[:long]
           perms.map do |p|
