65a86745e80198928d3f3748e589ccd8bccbab66
Richard Lister <rlister+gh@gmail.com>
2016-04-11 10:16:04 -0400

    add basic sqs list and dump tasks

---
+++ lib/awful/sqs.rb
@@ -0,0 +1,54 @@
+module Awful
+  module Short
+    def sqs(*args)
+      Awful::SQS.new.invoke(*args)
+    end
+  end
+
+  class SQS < Cli
+    no_commands do
+      def sqs
+        @sqs ||= Aws::SQS::Client.new
+      end
+
+      def is_url?(str)
+        str =~ /\A#{URI::regexp}\z/
+      end
+    end
+
+    desc 'ls [PATTERN]', 'list subnets [with any tags matching PATTERN]'
+    method_option :long, aliases: '-l', default: false, desc: 'Long listing'
+    def ls(prefix = nil)
+      queues = sqs.list_queues(queue_name_prefix: prefix).queue_urls
+      attr = %w[QueueArn ApproximateNumberOfMessages ApproximateNumberOfMessagesNotVisible LastModifiedTimestamp]
+      if options[:long]
+        queues.map do |queue|
+          sqs.get_queue_attributes(queue_url: queue, attribute_names: attr).attributes
+        end.tap do |list|
+          print_table list.map { |q|
+            [
+              q['QueueArn'].split(':').last,
+              q['ApproximateNumberOfMessages'],
+              q['ApproximateNumberOfMessagesNotVisible'],
+              Time.at(q['LastModifiedTimestamp'].to_i)
+            ]
+          }
+        end
+      else
+        queues.map { |q| q.split('/').last }.tap(&method(:puts))
+      end
+    end
+
+    desc 'dump NAME_OR_URL ...', 'get attributes for queues by queue name or url'
+    def dump(*names)
+      names.map do |name|
+        url = is_url?(name) ? name : sqs.get_queue_url(queue_name: name).queue_url
+        sqs.get_queue_attributes(queue_url: url, attribute_names: %w[All]).attributes
+      end.tap do |queues|
+        queues.each do |queue|
+          puts YAML.dump(stringify_keys(queue))
+        end
+      end
+    end
+  end
+end
