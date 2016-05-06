ab002259e84d9a9c26cda3caa915b244cab3ee8b
Richard Lister <rlister+gh@gmail.com>
2016-05-06 16:37:42 -0400

    basic cloudwatch logs support

---
+++ lib/awful/cloudwatch_logs.rb
@@ -0,0 +1,51 @@
+module Awful
+  module Short
+    def cwlogs(*args)
+      Awful::CloudWatchLogs.new.invoke(*args)
+    end
+  end
+
+  class CloudWatchLogs < Cli
+    no_commands do
+      def logs
+        @logs ||= Aws::CloudWatchLogs::Client.new
+      end
+    end
+
+    desc 'ls [PREFIX]', 'list log groups'
+    method_option :long, aliases: '-l', default: false, desc: 'Long listing'
+    def ls(prefix = nil)
+      next_token = nil
+      loop do
+        response = logs.describe_log_groups(log_group_name_prefix: prefix, next_token: next_token)
+        response.log_groups.tap do |groups|
+          if options[:long]
+            print_table groups.map { |group|
+              [
+                group.log_group_name,
+                group.retention_in_days,
+                Time.at(group.creation_time.to_i/1000),
+                group.stored_bytes,
+              ]
+            }
+          else
+            puts groups.map(&:log_group_name)
+          end
+        end
+        next_token = response.next_token
+        break if next_token.nil?
+      end
+    end
+
+    desc 'create NAME', 'create log group'
+    def create(name)
+      logs.create_log_group(log_group_name: name)
+    end
+
+    desc 'delete NAME', 'delete named log group'
+    def delete(name)
+      logs.delete_log_group(log_group_name: name)
+    end
+
+  end
+end
