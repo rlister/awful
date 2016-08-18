547547a9fda58d5e3cc963ae0e12b91a6802e335
Richard Lister <rlister+gh@gmail.com>
2016-08-18 17:24:00 -0400

    fix paging, and -p option, and add magical tail option

--- lib/awful/cloudwatch_logs.rb
+++ lib/awful/cloudwatch_logs.rb
@@ -100,26 +100,45 @@ module Awful
     end
 
     desc 'events GROUP [STREAM]', 'get log events from given, or latest, stream'
-    method_option :long,  aliases: '-l', default: false, desc: 'Long listing'
-    method_option :limit, aliases: '-n', default: 50,    desc: 'Count to limit returned results'
+    method_option :long,  aliases: '-l', type: :boolean, default: false,           desc: 'long listing including timestamps'
+    method_option :limit, aliases: '-n', type: :numeric, default: nil,             desc: 'limit results returned per page'
+    method_option :pages, aliases: '-p', type: :numeric, default: Float::INFINITY, desc: 'limit number of pages to output'
+    method_option :head,  aliases: '-H', type: :boolean, default: true,            desc: 'start from head, ie first page'
+    method_option :tail,  aliases: '-t', type: :numeric, default: nil,             desc: 'show given small number of most recent events'
     def events(group, stream = nil)
+      stream ||= latest_stream(group).log_stream_name # use latest stream if none requested
+
+      ## magical tail option returns up to requested number of last events
+      opt = options.dup
+      if opt[:tail]
+        opt[:head]  = false
+        opt[:pages] = 1
+        opt[:limit] = opt[:tail]
+      end
+
       next_token = nil
       events = []
+      pages = 0
       loop do
         response = logs.get_log_events(
-          log_group_name: group,
-          log_stream_name: stream || latest_stream(group).log_stream_name,
-          start_from_head: true,
-          limit: options[:limit],
-          next_token: next_token
+          log_group_name:  group,
+          log_stream_name: stream,
+          start_from_head: opt[:head],
+          limit:           opt[:limit],
+          next_token:      next_token,
         )
+
         break if response.events.count == 0 # break on no results as token does not get to nil
         events = events + response.events
+
+        pages += 1
+        break if pages >= opt[:pages]
+
         next_token = response.next_backward_token
-        break if events.count >= options[:limit].to_i
         break if next_token.nil? # not sure if this is ever set to nil
       end
-      events.tap do |ev|
+
+      events.output do |ev|
         if options[:long]
           print_table ev.map { |e| [Time.at(e.timestamp.to_i/1000), e.message] }
         else
