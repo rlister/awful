c6aaba5c28bfd5f7cd44d093e6548b38d8e6c40a
Richard Lister <ric@burton.local>
2016-10-12 17:50:03 -0400

    ipset get and update methods

--- lib/awful/waf_ipset.rb
+++ lib/awful/waf_ipset.rb
@@ -20,6 +20,37 @@ module Awful
         end
       end
 
+      desc 'get ID', 'get ipset'
+      method_option :long, aliases: '-l', type: :boolean, default: false, desc: 'long listing'
+      def get(id)
+        waf.get_ip_set(ip_set_id: id).ip_set.ip_set_descriptors.output do |ipsets|
+          if options[:long]
+            print_table ipsets.map { |i| [i.value, i.type] }
+          else
+            puts ipsets.map(&:value)
+          end
+        end
+      end
+
+      desc 'update ID', 'update ipset with ips'
+      method_option :type, type: :string, default: 'ipv4', desc: 'type of address, IPV4 or IPV6'
+      method_option :delete, type: :boolean, default: false, desc: 'delete IPs'
+      def update(id, *values)
+        waf.update_ip_set(
+          ip_set_id: id,
+          change_token: change_token,
+          updates: values.map do |value|
+            {
+              action: options[:delete] ? 'DELETE' : 'INSERT',
+              ip_set_descriptor: {
+                type: options[:type].upcase,
+                value: value
+              }
+            }
+          end
+        )
+      end
+
     end
 
     class Base < Cli
@@ -28,4 +59,4 @@ module Awful
     end
 
   end
-end
+ end
