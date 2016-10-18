294d8178fdd00e576a7dc9b6a34cce7ecb4e0c20
Richard Lister <rlister@gmail.com>
2016-10-18 16:01:54 -0400

    aliases task to show or update list of aliases for distro

--- lib/awful/cloudfront.rb
+++ lib/awful/cloudfront.rb
@@ -56,6 +56,28 @@ module Awful
       end
     end
 
+    desc 'aliases ID [LIST]', 'get/set aliases for distribution'
+    method_option :add,    aliases: '-a', type: :boolean, default: false, desc: 'add list to existing aliases'
+    method_option :delete, aliases: '-d', type: :boolean, default: false, desc: 'delete list from existing aliases'
+    def aliases(id, *list)
+      if list.empty?
+        cloudfront.get_distribution(id: id).distribution.distribution_config.aliases.items.output(&method(:puts))
+      else
+        response = cloudfront.get_distribution(id: id) # get current config
+        config = response.distribution.distribution_config
+        new = config[:aliases][:items]
+        if options[:add]
+          new += list           # append to existing
+        elsif options[:delete]
+          new -= list           # remove from existing
+        else
+          new = list            # replace existing
+        end
+        config[:aliases] = {quantity: new.count, items: new}
+        cloudfront.update_distribution(id: id, distribution_config: config, if_match: response.etag)
+      end
+    end
+
     desc 'origins ID', 'list origins for distribution with ID'
     method_option :long, aliases: '-l', type: :boolean, default: false, desc: 'long listing'
     def origins(id)
