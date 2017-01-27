ba8a4ceba66b779457a57d23d8c34cfe3c386ef5
Richard Lister <rlister@gmail.com>
2017-01-27 16:46:05 -0500

    cleanup ami tasks to take optional list of images

--- lib/awful/ami.rb
+++ lib/awful/ami.rb
@@ -6,8 +6,8 @@ module Awful
   end
 
   class Ami < Cli
-    class_option :owners,  aliases: '-o', type: :string, default: 'self', desc: 'List images with this owner'
-    class_option :filters, aliases: '-f', type: :array,  default: [],     desc: 'Filter using name=value, eg tag:Foo=bar, multiples are ANDed'
+    class_option :owners,  aliases: '-o', type: :array, default: %w[self], desc: 'List images with this owner'
+    class_option :filters, aliases: '-f', type: :array, default: [],       desc: 'Filter using name=value, eg tag:Foo=bar, multiples are ANDed'
 
     COLORS = {
       available: :green,
@@ -16,15 +16,17 @@ module Awful
     }
 
     no_commands do
-      def images(options)
+      def images(*image_ids)
         params = {
-          owners:  options[:owners].split(','),
+          image_ids: Array(image_ids),
+          owners:  options[:owners],
           filters: options[:filters].map do |tag|
             k, v = tag.split('=')
             {name: k, values: v.split(',')}
           end
         }.reject { |k,v| v.empty? }
-        ec2.describe_images(params).map(&:images).flatten
+
+        ec2.describe_images(params).images
       end
 
       def color(string)
@@ -32,12 +34,10 @@ module Awful
       end
     end
 
-    desc 'ls [PATTERN]', 'list AMIs'
+    desc 'ls [IDS]', 'list AMIs'
     method_option :long, aliases: '-l', type: :boolean, default: false, desc: 'Long listing'
-    def ls(name = /./)
-      images(options).select do |image|
-        image.name.match(name)
-      end.output do |list|
+    def ls(*ids)
+      images(*ids).output do |list|
         if options[:long]
           print_table list.map { |i|
             [ i.name, i.image_id, i.root_device_type, color(i.state), i.creation_date, i.tags.map{ |t| "#{t.key}=#{t.value}" }.sort.join(',') ]
@@ -48,20 +48,17 @@ module Awful
       end
     end
 
-    desc 'delete NAME', 'delete AMI'
+    desc 'delete ID', 'delete AMI'
     def delete(id)
-      images(options).find do |image|
-        image.image_id.match(id)
-      end.output do |ami|
-        if yes? "Really deregister image #{ami.name} (#{ami.image_id})?", :yellow
-          ec2.deregister_image(image_id: ami.image_id)
-        end
+      ami = images(id).first
+      if yes? "Really deregister image #{ami.name} (#{ami.image_id})?", :yellow
+        ec2.deregister_image(image_id: ami.image_id)
       end
     end
 
     desc 'dump IDS', 'describe images'
     def dump(*ids)
-      ec2.describe_images(image_ids: ids).images.output do |images|
+      images(*ids).output do |images|
         images.each do |image|
           puts YAML.dump(stringify_keys(image.to_hash))
         end
@@ -71,8 +68,8 @@ module Awful
     desc 'tags ID [TAGS]', 'get tags for AMI, or set multiple tags as key:value'
     def tags(id, *tags)
       if tags.empty?
-        ec2.describe_images(image_ids: [id]).images.first.tags.output do |list|
-          print_table list.map { |t| [t.key, t.value] }
+        images(id).first.tags.output do |tags|
+          print_table tags.map { |t| [t.key, t.value] }
         end
       else
         ec2.create_tags(
@@ -100,7 +97,7 @@ module Awful
     desc 'last NAME', 'get ids of last (by creation date) AMIs [matching NAME]'
     method_option :count, aliases: '-n', type: :numeric, default: 1, desc: 'Return N results'
     def last(name = /./)
-      images(options).select do |image|
+      images.select do |image|
         image.name.match(name)
       end.sort_by(&:creation_date).last(options[:count]).map do |image|
         image.image_id
