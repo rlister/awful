7a2d94487bf835f513afac3f42ac0f5e6d5c25b9
Richard Lister <rlister@gmail.com>
2017-02-10 15:22:23 -0500

    add filters to ls command

--- lib/awful/param.rb
+++ lib/awful/param.rb
@@ -12,11 +12,17 @@ module Awful
       end
     end
 
-    desc 'ls', 'list parameters'
-    method_option :long, aliases: '-l', type: :boolean, default: false, desc: 'long listing'
-    def ls
+    desc 'ls [NAMES]', 'list parameters'
+    method_option :long,   aliases: '-l', type: :boolean, default: false, desc: 'long listing'
+    method_option :type,   aliases: '-t', type: :array,   default: nil,   desc: 'filter types: String, StringList, SecureString'
+    method_option :key_id, aliases: '-k', type: :array,   default: nil,   desc: 'filter key IDs'
+    def ls(*names)
+      filters = []
+      filters += [{key: 'Name',  values: names}]            unless names.empty?
+      filters += [{key: 'Type',  values: options[:type]}]   if options[:type]
+      filters += [{key: 'KeyId', values: options[:key_id]}] if options[:key_id]
       paginate(:parameters) do |token|
-        ssm.describe_parameters(next_token: token)
+        ssm.describe_parameters(filters: filters, next_token: token)
       end.output do |params|
         if options[:long]
           print_table params.map { |p|
