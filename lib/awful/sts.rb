92e848d673cf5ca059512380dd2a0491925c58b4
Richard Lister <rlister@gmail.com>
2017-06-09 14:50:20 -0400

    add --env option to output eval-able env vars from STS assumeRole

--- lib/awful/sts.rb
+++ lib/awful/sts.rb
@@ -13,13 +13,20 @@ module Awful
     end
 
     desc 'assume_role ARN NAME', 'Return credentials for given role'
-    method_option :duration_seconds, aliases: '-d', type: :numeric, default: nil, desc: 'Duration of session in sec'
-    method_option :policy,           aliases: '-p', type: :string,  default: nil, desc: 'Access policy as JSON string'
+    method_option :duration_seconds, aliases: '-d', type: :numeric, default: nil,   desc: 'Duration of session in sec'
+    method_option :policy,           aliases: '-p', type: :string,  default: nil,   desc: 'Access policy as JSON string'
+    method_option :env,              aliases: '-e', type: :boolean, default: false, desc: 'output as env vars to eval'
     def assume_role(arn, name)
       opts = only_keys_matching(symbolize_keys(options), %i[duration_seconds policy])
       params = { role_arn: arn, role_session_name: name }.merge(opts)
-      sts.assume_role(params).tap do |session|
-        puts YAML.dump(stringify_keys(session.to_hash))
+      sts.assume_role(params).output do |session|
+        if options[:env]
+          puts %i[access_key_id secret_access_key session_token].map { |var|
+            "export AWS_#{var.upcase}=#{session.credentials.send(var)}"
+          }
+        else
+          puts YAML.dump(stringify_keys(session.to_hash))
+        end
       end
     end
   end
